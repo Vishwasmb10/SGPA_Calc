09c7c7f0d6e5960188936df1324c7ca832ed5548
Vishwasmb10 <vsmb.2410@gmail.com>
2024-04-10 10:49:10 +0530

    Added Calc.py

---
+++ Calc.py
@@ -0,0 +1,12 @@
+credit=[]
+grade=[]
+SGPA=0
+Sum=0
+n=int(input("Enter the number of subjects:\n"))
+print("Enter the credits and grade points of individual subjects:")
+for i in range(n):
+    credit.append(int(input(f"\nCredit of subject {i+1}: ")))
+    grade.append(int(input(f"Grade point of subject {i+1}: ")))
+    Sum+=(credit[i]*grade[i])
+SGPA=Sum/sum(credit)
+print(F"The SGAPA is {SGPA}")
